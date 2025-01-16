76fd08effa42a0c1423e96433f491b1e66747df8
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-16 16:52:32 -0600

    feat: update editor visibility logic to toggle between code and markdown views

--- src/main.js
+++ src/main.js
@@ -10,21 +10,30 @@ window.addEventListener("DOMContentLoaded", () => {
   // Toggle between code and markdown
   const codeButton = document.querySelector("#code-button");
   const mdButton = document.querySelector("#md-button");
+  
+  // Editors views
+  const codeEditor = document.querySelector("#code-editor");
+  const mdEditor = document.querySelector("#markdown-editor");
+
   // Search
-  const searchInput = document.querySelector("#search-input");
   const searchButton = document.querySelector("#search-button");
   
   codeButton.addEventListener("click", () => {
     codeButton.classList.add("active");
+    codeEditor.style.display = "grid";
     mdButton.classList.remove("active");
+    mdEditor.style.display = "none";
   });
 
   mdButton.addEventListener("click", () => {
     mdButton.classList.add("active");
+    mdEditor.style.display = "grid";
     codeButton.classList.remove("active");
+    codeEditor.style.display = "none";
   });
 
   searchButton.addEventListener("click", () => {
+    const searchInput = document.querySelector("#search-input");
     const query = searchInput.value;
     if (query) {
       files.forEach((file) => {
