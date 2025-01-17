11671f3b3c4f7aca58fe98eb68357b4d6fbfc65b
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-16 20:37:24 -0600

    refactor: modularize event handling functions for improved readability

--- src/main.js
+++ src/main.js
@@ -2,63 +2,69 @@ const { invoke } = window.__TAURI__.core; // Learn more about Tauri commands at 
 
 let filesName = document.querySelectorAll(".file-name");
 
+function changeMode(newMode, codeButton, mdButton) {
+  const codeEditor = document.querySelector("#code-editor");
+  const mdEditor = document.querySelector("#markdown-editor");
+
+  if (newMode === "code") {
+    codeButton.classList.add("active");
+    codeEditor.style.display = "grid";
+    mdButton.classList.remove("active");
+    mdEditor.style.display = "none";
+  } else if (newMode === "md") {
+    mdButton.classList.add("active");
+    mdEditor.style.display = "grid";
+    codeButton.classList.remove("active");
+    codeEditor.style.display = "none";
+  }
+}
+
+function search() {
+  const searchInput = document.querySelector("#search-input");
+  const query = searchInput.value;
+  if (query) {
+    filesName.forEach((file) => {
+      const fileName = file.textContent;
+      if (fileName.includes(query)) {
+        file.style.display = "block";
+      } else {
+        file.style.display = "none";
+      }
+    });
+  } else {
+    filesName.forEach((file) => {
+      file.style.display = "block";
+    });
+  }
+}
+
+function newFile() {
+  const fileName = prompt("Enter the file name (the md extension is added after the file is created):");
+  const filesContainer = document.querySelector("#files");
+  if (fileName) {
+    const file = document.createElement("button");
+    file.classList.add("file-name");
+    file.textContent = fileName;
+    filesContainer.appendChild(file);
+    filesName = document.querySelectorAll(".file-name");
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // Toggle between code and markdown
   const codeButton = document.querySelector("#code-button");
   const mdButton = document.querySelector("#md-button");
   
-  // Editors views
-  const codeEditor = document.querySelector("#code-editor");
-  const mdEditor = document.querySelector("#markdown-editor");
-
   // Search
   const searchButton = document.querySelector("#search-button");
 
   // Options
   const newFileButton = document.querySelector("#new-file");
   
-  codeButton.addEventListener("click", () => {
-    codeButton.classList.add("active");
-    codeEditor.style.display = "grid";
-    mdButton.classList.remove("active");
-    mdEditor.style.display = "none";
-  });
+  codeButton.addEventListener("click", () => { changeMode("code", codeButton, mdButton) });
+  mdButton.addEventListener("click", () => { changeMode("md", codeButton, mdButton) });
 
-  mdButton.addEventListener("click", () => {
-    mdButton.classList.add("active");
-    mdEditor.style.display = "grid";
-    codeButton.classList.remove("active");
-    codeEditor.style.display = "none";
-  });
+  searchButton.addEventListener("click", () => { search() });
 
-  searchButton.addEventListener("click", () => {
-    const searchInput = document.querySelector("#search-input");
-    const query = searchInput.value;
-    if (query) {
-      filesName.forEach((file) => {
-        const fileName = file.textContent;
-        if (fileName.includes(query)) {
-          file.style.display = "block";
-        } else {
-          file.style.display = "none";
-        }
-      });
-    } else {
-      filesName.forEach((file) => {
-        file.style.display = "block";
-      });
-    }
-  });
-
-  newFileButton.addEventListener("click", async () => {
-    const fileName = prompt("Enter the file name (the md extension is added after the file is created):");
-    const filesContainer = document.querySelector("#files");
-    if (fileName) {
-      const file = document.createElement("button");
-      file.classList.add("file-name");
-      file.textContent = fileName;
-      filesContainer.appendChild(file);
-      filesName = document.querySelectorAll(".file-name");
-    }
-  });
+  newFileButton.addEventListener("click", async () => { newFile() });
 });
